e07cc541c1fd0661f8a3c0e12751e0f9cabe3457
Adventune <50050797+Adventune@users.noreply.github.com>
2024-04-14 18:07:07 +0300

    feat: docker-engine-install-ubuntu.sh

---
+++ docker-engine-install-ubuntu.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# DOCKER INSTALL ON UBUNTU MACHINES
+
+## UNINSTALL OLD/CONFILICTING PACKAGES
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+## ADD DOCKER APT REPOSITORY
+
+### Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+### Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+## INSTALL PACKAGES
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+## ADD USER TO DOCKER GROUP
+sudo usermod -aG docker $USER
+
+echo =================================
+echo FINISHED!
+echo =================================
