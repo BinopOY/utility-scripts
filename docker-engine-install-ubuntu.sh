d2d0a546881325fa01deaae842b26ebaa4557d05
Adventune <50050797+Adventune@users.noreply.github.com>
2024-04-14 18:11:01 +0300

    fix: add -y

--- docker-engine-install-ubuntu.sh
+++ docker-engine-install-ubuntu.sh
@@ -22,7 +22,7 @@ echo \
 sudo apt-get update
 
 ## INSTALL PACKAGES
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 ## ADD USER TO DOCKER GROUP
 sudo usermod -aG docker $USER
